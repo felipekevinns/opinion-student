b360d640eea0c08ce1f63b41f1e184eb8aeb1540
Felipe Kevin <felipekevinns.2006@gmail.com>
2025-11-17 14:58:23 -0300

    Corrige CORS para aceitar requests PUT

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ const app = express();
 const PORT = 3000;
 
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 
 app.use(express.json());
